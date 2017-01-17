18f45888454be1857241dddd37bb9cc32a00cbed
Ryan Volz <rvolz@mit.edu>
2017-01-16 22:04:02 -0500

    Comment relay numbers with devices they control.

--- scripts/activate_relays.py
+++ scripts/activate_relays.py
@@ -14,11 +14,11 @@ print("activating relays...")
 
 relay = th[0]
 
-relay.set_relay_position(1,1)
+relay.set_relay_position(1,1)  # drives
 time.sleep(1)
-relay.set_relay_position(2,1)
+relay.set_relay_position(2,1)  # GPS
 time.sleep(1)
-relay.set_relay_position(3,1)
+relay.set_relay_position(3,1)  # amps
 time.sleep(1)
-relay.set_relay_position(4,1)
+relay.set_relay_position(4,1)  # USRP
 time.sleep(1)
--- scripts/deactivate_relays.py
+++ scripts/deactivate_relays.py
@@ -14,11 +14,11 @@ print("de-activating relays...")
 
 relay = th[0]
 
-relay.set_relay_position(1,0)
+relay.set_relay_position(1,0)  # drives
 time.sleep(1)
-relay.set_relay_position(2,0)
+relay.set_relay_position(2,0)  # GPS
 time.sleep(1)
-relay.set_relay_position(3,0)
+relay.set_relay_position(3,0)  # amps
 time.sleep(1)
-relay.set_relay_position(4,0)
+relay.set_relay_position(4,0)  # USRP
 time.sleep(1)
